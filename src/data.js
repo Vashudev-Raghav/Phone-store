4e93a8949ae547ac4ec1b63cff1f0ebde2ebed82
VASHUDEV-RAGHAV <devraghav81@gmail.com>
2020-04-13 17:08:28 +0530

    first Commit

--- src/data.js
+++ src/data.js
@@ -4,8 +4,58 @@ Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk */
 
 export const storeProducts = [
-  {
+   {
     id: 1,
+    title: "Samsung S20 Ultra",
+    img: "img/product-12.png",
+    price: 1000,
+    company: "Samsung",
+    info:
+      //"Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+          "Samsung Galaxy S20 Ultra smartphone was launched on 11th February 2020. The phone comes with a 6.90-inch touchscreen display with a resolution of 1440x3200 pixels and an aspect ratio of 20:9. Samsung Galaxy S20 Ultra is powered by a 2GHz octa-core Samsung Exynos 990 processor that features 2 cores clocked at 2.73GHz, 2 cores clocked at 2.5GHz and 4 cores clocked at 2GHz. It comes with 12GB of RAM. The Samsung Galaxy S20 Ultra runs Android 10 and is powered by a 5000mAh non-removable battery. The Samsung Galaxy S20 Ultra supports wireless charging, as well as proprietary fast charging.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Realme 6 pro",
+    img: "img/product-9.png",
+    price: 250,
+    company: "Realme",
+    info:
+      "The phone comes with a 6.60-inch touchscreen display with a resolution of 1080x2400 pixels and an aspect ratio of 20:9. Realme 6 Pro is powered by an octa-core Qualcomm Snapdragon 720G processor. It comes with 6GB of RAM. The Realme 6 Pro runs Android 10 and is powered by a 4300mAh non-removable battery. The Realme 6 Pro supports proprietary fast charging.As far as the cameras are concerned, the Realme 6 Pro on the rear packs a 64-megapixel primary camera with an f/1.8 aperture; a second 8-megapixel camera with an f/2.3 aperture; a third 12-megapixel camera with an f/2.5 aperture and a fourth 2-megapixel camera with an f/2.4 aperture. The rear camera setup has autofocus. On the front, the Realme 6 Pro packs a 16-megapixel primary camera with an f/2.0 aperture and a second 8-megapixel camera with an f/2.2 apert",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Redmi note 9 Pro",
+    img: "img/product-10.png",
+    price: 200,
+    company: "Xiaomi",
+    info:
+      //"Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+          "Xiaomi Redmi Note 9 Pro smartphone runs on Android v10 (Q) operating system. The phone is powered by Octa core (2.3 GHz, Dual core, Kryo 465 + 1.8 GHz, Hexa Core, Kryo 465) processor. It runs on the Qualcomm Snapdragon 720G Chipset. It has 4 GB RAM and 64 GB internal storage Xiaomi Redmi Note 9 Pro smartphone has a IPS LCD display. It measures 165.7 mm x 76.6 mm x 8.8 mm and weighs 209 grams. The screen has a resolution of 1080 x 2400 pixels and 395 ppi pixel density. It has an aspect ratio of 20:9 and screen-to-body ratio of 84.62 %. On camera front, the buyers get a 16 MP Primary Camera(1µm pixel size) and on the rear, there's an 48+8+5+2 MP camera with features like 10 x Digital Zoom, Auto Flash, Face detection, Touch to focus. It is backed by a 5020 mAh battery. Connectivity features in the smartphone include WiFi, Bluetooth, GPS, Volte, and more.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+   {
+    id: 4,
+    title: "Samsung s10",
+    img: "img/product-11.png",
+    price: 1000,
+    company: "Samsung",
+    info:
+      "The phone comes with a 6.10-inch touchscreen display and an aspect ratio of 19:9. Samsung Galaxy S10 is powered by a 1.9GHz octa-core Samsung Exynos 9820 processor. It comes with 8GB of RAM. The Samsung Galaxy S10 runs Android 9.0 and is powered by a 3400mAh non-removable battery. The Samsung Galaxy S10 supports wireless charging, as well as proprietary fast charging.As far as the cameras are concerned, the Samsung Galaxy S10 on the rear packs a 12-megapixel primary camera with an f/1.5 aperture; a second 12-megapixel camera with an f/2.4 aperture and a third 16-megapixel camera with an f/2.2 aperture. The rear camera setup has phase detection autofocus. It sports a 10-megapixel camera on the front for selfies, with an f/1.9 aperture. The front camera also features autofocus.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
     title: "Google Pixel - Black",
     img: "img/product-1.png",
     price: 10,
@@ -18,7 +68,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 2,
+    id: 6,
     title: "Samsung S7",
     img: "img/product-2.png",
     price: 16,
@@ -30,7 +80,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 3,
+    id: 7,
     title: "HTC 10 - Black",
     img: "img/product-3.png",
     price: 8,
@@ -42,7 +92,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 4,
+    id: 8,
     title: "HTC 10 - White",
     img: "img/product-4.png",
     price: 18,
@@ -54,7 +104,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 5,
+    id: 9,
     title: "HTC Desire 626s",
     img: "img/product-5.png",
     price: 24,
@@ -66,7 +116,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 6,
+    id: 10,
     title: "Vintage Iphone",
     img: "img/product-6.png",
     price: 17,
@@ -78,7 +128,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 7,
+    id: 11,
     title: "Iphone 7",
     img: "img/product-7.png",
     price: 30,
@@ -90,7 +140,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 8,
+    id: 12,
     title: "Smashed Iphone",
     img: "img/product-8.png",
     price: 2,
